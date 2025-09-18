145ac13d8fdfb0d766c3f04c8e7700af6a7c6291
Ilya <skrontex@gmail.com>
2025-09-18 23:16:52 +0300

    Добавлены подборки событий  - после добавление ветки event, пересмотреть тесты и импорты

---
+++ main/src/main/java/ru/practicum/ewm/model/Compilation.java
@@ -0,0 +1,34 @@
+package ru.practicum.ewm.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@ToString
+@Table(name = "compilations")
+public class Compilation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "pinned", nullable = false)
+    private Boolean pinned;
+
+    @ManyToMany
+    @JoinTable(
+            name = "compilation_events",
+            joinColumns = @JoinColumn(name = "compilation_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id")
+    )
+    private Set<Event> events = new HashSet<>();
+}
