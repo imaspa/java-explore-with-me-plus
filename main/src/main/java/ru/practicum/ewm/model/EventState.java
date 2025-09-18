84ee4d016d5abe8134a75de01b984814e2956f05
Aspa <kreoll@gmail.com>
2025-09-18 14:25:40 +0300

    Revert "Main svc events (#10)"
    This reverts commit dc2d93ce291011f5d9a6318759ee68e8f74a4292.

--- main/src/main/java/ru/practicum/ewm/model/EventState.java
+++
@@ -1,5 +0,0 @@
-package ru.practicum.ewm.model;
-
-public enum EventState {
-    PENDING, PUBLISHED, CANCELED
-}
--- main/src/main/java/ru/practicum/ewm/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.ewm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.ewm.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
