3c7f9ff8a89fa1d4793a98bb4bf3ff2d9eb00f96
imaspa <yaaspa@ya.ru>
2025-09-18 14:28:20 +0300

    Event - модели и dto (#11)
    Co-authored-by: avv <avv37@yandex.com>

---
+++ main/src/main/java/ru/practicum/ewm/model/Location.java
@@ -0,0 +1,26 @@
+package ru.practicum.ewm.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity
+@ToString
+@Table(name = "locations")
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "lat", nullable = false)
+    private Float lat;
+    @Column(name = "lon", nullable = false)
+    private Float lon;
+}
