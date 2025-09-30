807644e0b74b64dcf4d2c0916dd19f3aec8c0562
Александр Викторов <Novacancy531@yandex.ru>
2025-09-30 11:58:55 +0300

    Правки согласно замечаниям.

--- main/src/main/java/ru/practicum/ewm/dto/comment/CommentDto.java
+++ main/src/main/java/ru/practicum/ewm/dto/comment/CommentDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.ewm.dto.comment;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,8 +17,6 @@ public class CommentDto {
     private String authorName;
     private Long event;
     private String text;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime created;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime updated;
 }
